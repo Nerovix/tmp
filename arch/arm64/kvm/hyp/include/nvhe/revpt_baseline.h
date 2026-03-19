9698f264743d3fbd59ba9663bc38fe3cc1c7e328
Nerovix <152875431+Nerovix@users.noreply.github.com>
2026-03-19 21:39:31 +0800

    kvm: nvhe: switch revpt to incremental post-start updates

--- arch/arm64/kvm/hyp/include/nvhe/revpt_baseline.h
+++ arch/arm64/kvm/hyp/include/nvhe/revpt_baseline.h
@@ -4,6 +4,7 @@
 
 #include <linux/types.h>
 #include <asm/page.h>
+#include <nvhe/pfn_info.h>
 
 struct revpt_test_cfg {
 	bool configured;
@@ -24,5 +25,21 @@ bool revpt_pa_overlap_enabled(phys_addr_t pa, u64 size,
  * - 遍历页表并重建 rev_pt 的 CPU 可达性基线
  */
 int revpt_capture_cpu_baseline_locked(u64 *cpu_snapshot_pages);
+
+int revpt_apply_share_locked(phys_addr_t phys_start, u64 nr_pages,
+			     enum page_owner owner_comp,
+			     enum page_owner borrower_comp);
+int revpt_apply_unshare_locked(phys_addr_t phys_start, u64 nr_pages,
+			       enum page_owner owner_comp,
+			       enum page_owner borrower_comp);
+int revpt_apply_donate_locked(phys_addr_t phys_start, u64 nr_pages,
+			      enum page_owner from_comp,
+			      enum page_owner to_comp);
+int revpt_apply_host_cpu_map_locked(phys_addr_t phys_start, u64 size);
+int revpt_apply_host_cpu_unmap_locked(phys_addr_t phys_start, u64 size);
+int revpt_apply_hyp_cpu_map_locked(phys_addr_t phys_start, u64 size);
+int revpt_apply_hyp_cpu_unmap_locked(phys_addr_t phys_start, u64 size);
+int revpt_apply_host_dma_map_locked(phys_addr_t phys_start, u64 size);
+int revpt_apply_host_dma_unmap_locked(phys_addr_t phys_start, u64 size);
 
 #endif
